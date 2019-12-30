63c4a6c72e26bdc10fcc3ddb23cfb1278c17bfee
csy <kisscsy@foxmail.com>
2019-12-30 15:14:25 +0800

    20191230-1514-搭建完框架

---
+++ src/store/reducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux-immutable';
+// import { reducer as todolistReducer } from '../pages/todolist/store/index.js';
+// import { reducer as headerReducer } from '../common/header/store/index.js';
+// import { reducer as homeReducer } from '../pages/home/store';
+// import { reducer as detailReducer } from '../pages/detail/store';
+// import { reducer as loginReducer } from '../pages/login/store';
+
+const reducer = combineReducers({
+  // todolist: todolistReducer,
+  // header: headerReducer,
+  // home: homeReducer,
+  // detail: detailReducer,
+  // login: loginReducer
+})
+
+export default reducer
